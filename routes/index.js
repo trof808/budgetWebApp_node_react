3b336a44255a6b8211c6fd7229b2df49bad399f3
trof <reatrof@gmail.com>
2017-05-15 13:59:50 +0300

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const passport = require('passport');
 const route = express.Router();
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+const request = require('request');
 
 var options = {}
 
@@ -8,15 +11,23 @@ route.get('/', (req, res, next) => {
 });
 
 route.get('/login', (req, res, next) => {
-	res.render('user', {options: options});
+	res.render('login', {options: options});
 });
 
 route.get('/logout', (req, res, next) => {
-	res.send('Страница выхода');
+	req.logout();
+  res.redirect('/');
 });
 
 route.get('/polls', (req, res, next) => {
-	res.render('polls', {user: user});
+	request('http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president', (error, response, body) => {
+		if(!error && response.statusCode == 200) {
+			let polls = JSON.parse(body);
+			res.render('polls', {options: options, user: req.user, polls: polls});
+		} else {
+			render('error');
+		}
+	})
 });
 
 route.get('/user', (req, res, next) => {
@@ -24,7 +35,7 @@ route.get('/user', (req, res, next) => {
 		nickname: 'Nikita',
 		picture: 'http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg'
 	}
-	res.render('user', {user: user});
+	res.render('user', {user: req.user});
 });
 
 module.exports = route;
