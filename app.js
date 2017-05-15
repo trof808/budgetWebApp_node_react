8df1065bab19b8d368136db803d8e3bfd7b3a1f1
trof <reatrof@gmail.com>
2017-05-15 09:18:00 +0300

    main file app

---
+++ app.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const config = require('./config');
+
+dotenv.load();
+
+const routes = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+	secret: config.cookieSecret,
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+	var err = new Error('not found');
+	err.status(404);
+	err.next();
+});
+
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.send(err);
+});
+
+app.listen(3000);
+
