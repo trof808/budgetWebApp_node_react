47e6def3635b5fd4641766fc3968d80a292aa17f
trof <reatrof@gmail.com>
2017-05-16 00:24:19 +0300

    redis added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const passport = require('passport');
 const route = express.Router();
 const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 const request = require('request');
+const redis = require("redis");
+const client = redis.createClient();
 
 var env = {
 	AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
@@ -11,7 +13,19 @@ var env = {
 }
 
 route.get('/', (req, res, next) => {
-	res.render('index', {env: env});
+	if(req.user) {
+		res.redirect('/polls');
+	} else {
+		res.render('index', {env: env});
+	}
+});
+
+route.get('/user/:username/start', (req, res, next) => {
+	res.render('start', {user: user});
+});
+
+route.get('/user/:username', (req, res, next) => {
+	res.send('Ваш провиль, ' + req.user.nickname);
 });
 
 route.get('/login', (req, res, next) => {
@@ -23,10 +37,11 @@ route.get('/logout', (req, res, next) => {
   res.redirect('/');
 });
 
-route.get('/polls', (req, res, next) => {
+route.get('/polls', ensureLoggedIn, (req, res, next) => {
 	request('http://elections.huffingtonpost.com/pollster/api/charts.json?topic=2016-president', (error, response, body) => {
 		if(!error && response.statusCode == 200) {
 			let polls = JSON.parse(body);
+			console.log(req.user.identities[0].user_id);
 			res.render('polls', {env: env, user: req.user, polls: polls});
 		} else {
 			render('error');
@@ -35,17 +50,26 @@ route.get('/polls', (req, res, next) => {
 });
 
 route.get('/user', (req, res, next) => {
-	let user = {
-		nickname: 'Nikita',
-		picture: 'http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg'
-	}
+	console.log(req.user);
 	res.render('user', {user: req.user});
 });
 
 route.get('/callback',
 	passport.authenticate('auth0', { failureRedirect: '/' }),
-	function(req, res) {
-	  res.redirect(req.session.returnTo || '/polls');
+	(req, res, next) => {
+		client.exists('user_id', (err, reply) => {
+			if(reply === 1) {
+				console.log('Такой пользователь уже есть: ' + client.get('user_id'));
+				next();
+			} else {
+				client.set('user_id', req.user.identities[0].user_id);
+				console.log('Пользователь добавлен: ' + client.get('user_id'));
+				res.redirect('/user/'+req.user.nickname+'/start');
+			}
+		});
+	},
+	(req, res) => {
+	  res.redirect(req.session.returnTo || '/user/'+req.user.nickname);
 });
 
 module.exports = route;
